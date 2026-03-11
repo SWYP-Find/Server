43d01c647f8b6b98a9ed7d91b05dde0251bef74e
jucheonsu <jucheonsu@gmail.com>
2026-03-12 02:04:22 +0900

    🔨 Chore: 프로젝트 초기 환경 및 Docker DB, Security, Swagger 설정 구축

--- src/main/java/com/swyp/app/AppApplication.java
+++ src/main/java/com/swyp/app/AppApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
 @EnableJpaAuditing
+@SpringBootApplication
 public class AppApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
 }

980b9248fa485a5a6c584e0bb341d3cbde3c7c79
darren <gw8413@gmail.com>
2026-03-21 13:10:35 +0900

    Revert "#37 [Refactor] 홈 배틀 조회 V2 적용"
    This reverts commit 284a8a1a48bd0d5136dab7bf2dd0671f3cc9ce73.

--- src/main/java/com/swyp/app/domain/battle/repository/BattleOptionRepository.java
+++ src/main/java/com/swyp/app/domain/battle/repository/BattleOptionRepository.java
@@ -4,8 +4,6 @@ import com.swyp.app.domain.battle.entity.Battle;
 import com.swyp.app.domain.battle.entity.BattleOption;
 import com.swyp.app.domain.battle.enums.BattleOptionLabel;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,8 +12,6 @@ import java.util.UUID;
 public interface BattleOptionRepository extends JpaRepository<BattleOption, UUID> {
 
     List<BattleOption> findByBattle(Battle battle);
-    @Query("SELECT battleOption FROM BattleOption battleOption WHERE battleOption.battle IN :battles ORDER BY battleOption.battle.id ASC, battleOption.label ASC")
-    List<BattleOption> findByBattleInOrderByBattleIdAscLabelAsc(@Param("battles") List<Battle> battles);
     Optional<BattleOption> findByBattleAndLabel(Battle battle, BattleOptionLabel label);
 
 }

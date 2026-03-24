2d7759bcbbc7242793782928e83b31de963cf2bc
si-zero <snm306@naver.com>
2026-03-24 10:29:54 +0900

    feat: 회원탈퇴 DTO 추가

---
+++ src/main/java/com/swyp/app/domain/oauth/dto/WithdrawResponse.java
@@ -0,0 +1,13 @@
+package com.swyp.app.domain.oauth.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class WithdrawResponse {
+    private final boolean withdrawn;
+}

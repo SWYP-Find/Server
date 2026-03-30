0fb2c573c16bb9153192d3a3a1c69ab057f04201
si-zero <snm306@naver.com>
2026-03-30 21:52:35 +0900

    refactor: 스프링이 찾을 수 있도록 직접 지정

--- src/main/java/com/swyp/picke/domain/reward/dto/request/AdMobRewardRequest.java
+++ src/main/java/com/swyp/picke/domain/reward/dto/request/AdMobRewardRequest.java
@@ -1,8 +1,10 @@
 package com.swyp.picke.domain.reward.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.swyp.picke.domain.reward.enums.RewardItem;
 import com.swyp.picke.global.common.exception.CustomException;
 import com.swyp.picke.global.common.exception.ErrorCode;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public record AdMobRewardRequest(
         String ad_unit_id,
@@ -14,6 +16,26 @@ public record AdMobRewardRequest(
         String signature,
         String key_id
 ) {
+    // 직접 지정
+    public AdMobRewardRequest(
+            @RequestParam("ad_unit_id") String ad_unit_id,
+            @RequestParam("custom_data") String custom_data,
+            @RequestParam("reward_amount") int reward_amount,
+            @RequestParam("reward_item") String reward_item,
+            @RequestParam("timestamp") long timestamp,
+            @RequestParam("transaction_id") String transaction_id,
+            @RequestParam("signature") String signature,
+            @RequestParam("key_id") String key_id
+    ) {
+        this.ad_unit_id = ad_unit_id;
+        this.custom_data = custom_data;
+        this.reward_amount = reward_amount;
+        this.reward_item = reward_item;
+        this.timestamp = timestamp;
+        this.transaction_id = transaction_id;
+        this.signature = signature;
+        this.key_id = key_id;
+    }
     // 구글이 보낸 유저 데이터를 우리 데이터베이스에서 찾기 위해 메소드 추가
     public Long getUserId() {
         try {

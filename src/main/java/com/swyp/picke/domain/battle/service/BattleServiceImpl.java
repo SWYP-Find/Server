da12716d57247c959c1f703743843953d15df72d
Youwol <153346797+si-zero@users.noreply.github.com>
2026-04-23 15:40:32 +0900

    #198 [Feat] 오늘의 배틀 출력 개수 수정

--- src/main/java/com/swyp/picke/domain/battle/service/BattleServiceImpl.java
+++ src/main/java/com/swyp/picke/domain/battle/service/BattleServiceImpl.java
@@ -177,11 +177,11 @@ public class BattleServiceImpl implements BattleService {
     @Transactional
     public TodayBattleListResponse getTodayBattles() {
         LocalDate today = LocalDate.now();
-        ensureTodayPicks(today, 5);
+        ensureTodayPicks(today, 3);
         List<Battle> battles = battleRepository.findByTargetDateAndStatusAndDeletedAtIsNull(today, BattleStatus.PUBLISHED);
 
         List<Battle> limitedBattles = battles.stream()
-                .limit(5)
+                .limit(3)
                 .collect(Collectors.toList());
 
         List<TodayBattleResponse> items = convertToTodayResponses(limitedBattles);

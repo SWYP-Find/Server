a96763e9004a6d4dafaf994deae52e2eefd65864
si-zero <snm306@naver.com>
2026-06-18 23:12:44 +0900

    chore: AdMobConfig iOS/Android 광고 단위 ID 주입 추가

--- src/main/java/com/swyp/picke/global/config/AdMobConfig.java
+++ src/main/java/com/swyp/picke/global/config/AdMobConfig.java
@@ -1,11 +1,26 @@
 package com.swyp.picke.global.config;
 
 import com.google.crypto.tink.apps.rewardedads.RewardedAdsVerifier;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
+@Getter
 @Configuration
 public class AdMobConfig {
+
+    @Value("${admob.reward.unit-id.ios}")
+    private String iosRewardUnitId;
+
+    @Value("${admob.reward.unit-id.android}")
+    private String androidRewardUnitId;
+
+    public List<String> getAllowedUnitIds() {
+        return List.of(iosRewardUnitId, androidRewardUnitId);
+    }
 
     @Bean
     public RewardedAdsVerifier rewardedAdsVerifier() {

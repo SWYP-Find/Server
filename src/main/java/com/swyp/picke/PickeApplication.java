9f23f7a8a91dd4e1ad122d83fb00b5caa71ad723
Youwol <153346797+si-zero@users.noreply.github.com>
2026-07-06 09:44:10 +0900

    #299 [Merge] v2.0.11 업데이트

--- src/main/java/com/swyp/picke/PickeApplication.java
+++ src/main/java/com/swyp/picke/PickeApplication.java
@@ -1,5 +1,6 @@
 package com.swyp.picke;
 
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -10,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 public class PickeApplication {
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(PickeApplication.class, args); // 실행 클래스 수정완료
     }
 }
